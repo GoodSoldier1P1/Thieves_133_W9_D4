06acdd9f1489cbdbc07db14db3d6180468469032
GoodSoldier1P1 <danielamyx859@gmail.com>
2023-12-11 14:37:42 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import BibleSearch from './BibleSearch/BibleSearch'
+
+function App() {
+
+  return (
+    <>
+    <BibleSearch />
+    </>
+  )
+}
+
+export default App
---
+++ src/BibleSearch/BibleSearch.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react"
+
+interface Bible {
+    verseText: string,
+    book: string,
+    chapter: number,
+    verse: number
+}
+
+const BibleSearch = () => {
+
+    const [verse, setVerse] = useState<Bible>({
+        verseText: '',
+        book: '',
+        chapter: 0,
+        verse: 0
+    })
+
+    useEffect(() => {
+        getVerse()
+    }, [verse.verseText])
+
+    const getVerse = async () => {
+        const response = await fetch(`https://cors-anywhere.herokuapp.com/https://bible-api.com/${verse.verseText}`)    // had  CORS error. This was the fix I found
+        // Working on 403 error
+        if (response.ok) {
+            const data = await response.json()
+            setVerse({
+                verseText: data.text,
+                book: data.verses[0].book_name,
+                chapter: data.verses[0].chapter,
+                verse: data.verses[0].verse
+            })
+        }
+        else {
+            setVerse({ ...verse, verseText: '' })
+        }
+    }
+    return (
+        <>
+            <h1 className="text-center">What verse would you like to search?</h1>
+            <div className="w-25 mx-auto">
+                <input onChange={(event) => { setVerse({ ...verse, verseText: event.target.value }) }} type="text" />
+            </div>
+
+            {verse.verseText &&
+                <div className="card" style={{ width: '18rem' }}>
+                    <div className="card-body">
+                        <h5 className="card-title">{verse.book}</h5>
+                        <h6 className="card-subtitle mb-2 text-body-secondary">{verse.chapter}: {verse.verse}</h6>
+                        <p className="card-text">{verse.verseText}</p>
+                    </div>
+                </div>
+            }
+        </>
+    )
+}
+export default BibleSearch
